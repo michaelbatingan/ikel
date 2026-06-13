8e6a4c55968503f86ceeb0cc48061b72ff64f8d6
CIITSHSDTP <ciitshsdtp30@ciit.edu.ph>
2026-06-13 12:17:08 +0800

    morning session

---
+++ app/Http/Controllers/PensionerController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pensioner;
+use Illuminate\Http\Request;
+
+class PensionerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        try {
+            $pensioner = Pensioner::all();
+            //select * from pensioner
+            $response = [
+                'success' => true,
+                'data' => $pensioner,
+                'message' => 'Pensioner fetched successfully.'
+            ];
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred while fetching pensioner.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        try {
+            $validatedData = $request->validate([
+                'serial_number' => 'required|string|max:10',
+                'control_number' => 'required|string|max:20',
+                'first_name' => 'required|string|max:255',
+                'middle_name' => 'nullable|string|max:255',
+                'last_name' => 'required|string|max:255',
+                'pension_account' => 'required|string|max:20',
+                'rank' => 'nullable|string|max:20',
+                'bank_name' => 'required|string|max:50',
+                'amount_centavos' => 'required|bigInteger|default:0',
+                'retirement_date' => 'required|date',
+            ]);
+
+            $pensioner = Pensioner::create($validatedData);
+
+            return response()->json($pensioner, 201);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred while saving pensioner.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+         try {
+            $pensioner = Pensioner::findOrFail($id);
+            return response()->json($pensioner, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred while fetching pensioner.',
+                'pensioner_id' => $id,
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+          try {
+            $pensioner = Pensioner::findOrFail($id);
+
+            $validatedData = $request->validate([
+                'serial_number' => 'required|string|max:10',
+                'control_number' => 'required|string|max:20',
+                'first_name' => 'required|string|max:255',
+                'middle_name' => 'nullable|string|max:255',
+                'last_name' => 'required|string|max:255',
+                'pension_account' => 'required|string|max:20',
+                'rank' => 'nullable|string|max:20',
+                'bank_name' => 'required|string|max:50',
+                'amount_centavos' => 'required|bigInteger|default:0',
+                'retirement_date' => 'required|date',
+            ]);
+
+            $pensioner->update($validatedData);
+
+            return response()->json($pensioner, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred while updating pensioner.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        try {
+            $pensioner = Pensioner::findOrFail($id);
+            $pensioner->delete();
+
+            return response()->json([
+                'message' => 'Pensioner deleted successfully.',
+                'pensioner_id' => $id
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred while deleting pensioner.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+    return 'Hello, from Laravel!';
+});
+
+//http://localhost:8000/api/employees
+Route::get('/employees', [\App\Http\Controllers\EmployeeController::class, 'index']);
+Route::post('/employees', [\App\Http\Controllers\EmployeeController::class, 'store']);
+//http://localhost:8000/api/employees/2
+Route::get('/employees/{id}', [\App\Http\Controllers\EmployeeController::class, 'show']);
+Route::put('/employees/{id}', [\App\Http\Controllers\EmployeeController::class, 'update']);
+Route::delete('/employees/{id}', [\App\Http\Controllers\EmployeeController::class, 'destroy']);
+
+//http://localhost:8000/api/pensioner
+Route::get('/pensioners', [\App\Http\Controllers\PensionerController::class, 'index']);
+Route::post('/pensioners', [\App\Http\Controllers\PensionerController::class, 'store']);
+//http://localhost:8000/api/pensioner/2
+Route::get('/pensioners/{id}', [\App\Http\Controllers\PensionerController::class, 'show']);
+Route::put('/pensioners/{id}', [\App\Http\Controllers\PensionerController::class, 'update']);
+Route::delete('/pensioners/{id}', [\App\Http\Controllers\PensionerController::class, 'destroy']);
